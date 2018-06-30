98273ea2bf76be12757cf2867c027278064a2178
Maciej Zwoliński <34972409+Zwo1in@users.noreply.github.com>
2018-06-30 20:21:24 +0200

    refactoring code

--- matrix.hpp
+++ matrix.hpp
@@ -1,9 +1,10 @@
 #ifndef MATRIX_HPP_
-#deifne MATRIX_HPP_
+#define MATRIX_HPP_
 
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cmath>
 
 template <class U>
 class Matrix {
@@ -12,140 +13,193 @@ class Matrix {
 
     public:
     Matrix(){}
-    Matrix( std::initializer_list< std::initializer_list<U> > content ) {
-        rows = content.size();
-        cols = content.begin() -> size();
-        matrix.resize(rows);
-        auto j = content.begin();
-        for ( int i = 0; i < rows; ++i, ++j )
-            matrix[i].assign( *j );
-    }
+    // Construct from ( {{x,y},{z,x}} ) list
+    Matrix( std::initializer_list< std::initializer_list<U> > );
+    Matrix( int, int );
+    //returns size
+    std::pair<int, int> getSize();
+    //change shape, may corrupt data
+    void reshape( int, int );
+    // fill with given value
+    void fill( U );
+    // randomize content beetwen lower & upper bound
+    void randomize( U , U = 0 );
+    // transpose self
+    void transpose();
+    // return transposed
+    Matrix T();
+    // random access
+    std::vector<U>& operator[] ( unsigned int );
+    // math operators
+    Matrix operator* ( float );
+    Matrix operator* ( int );
+    Matrix operator* ( Matrix );
+    Matrix operator+ ( Matrix );
+    Matrix operator- ( Matrix );
 
-    Matrix( int rows_, int cols_ ) {
-        rows = rows_;
-        cols = cols_;
+    template <class V>
+    friend std::ostream& operator<< ( std::ostream& os, const Matrix<V>& target );
 
-        matrix.resize( rows );
-        for ( auto& row : matrix )
-            row.resize( cols );
-    }
-    
-    std::pair<int, int> getSize() {
-        return std::pair(rows,cols);
-    }
-
-    void reshape( int n, int m ) {
-        rows = n;
-        cols = m;
-        matrix.resize(n);
-        for ( auto& row : matrix )
-            row.resize(m);
-    }
-
-    void fill( U value ) {
-        for ( auto& row : matrix )
-            for ( auto& val : row )
-                val = value;
-    }
-
-    void randomize( U upper_bound, U lower_bound = 0 ) {
-        for ( auto& row : matrix )
-            for ( auto& value : row )
-                value = float(rand()%1000) / 1000 * (upper_bound - lower_bound) + lower_bound;
-    }
-
-    void transpose() {
-        Matrix result(cols, rows);
-        for ( int i = 0; i < rows; ++i ) {
-            for ( int j = 0; j < cols; ++j ) {
-                result[j][i] = (*this)[i][j];
-            }
-        }
-        *this = result;
-    }
-
-    Matrix T() {
-        auto result = *this;
-        result.transpose();
-        return result;
-    }
-
-    std::vector<U>& operator[] ( unsigned int n ) {
-        return matrix[n];
-    }
-    Matrix operator* ( float a ) {
-        Matrix result(rows, cols);
-        for ( int i = 0; i < rows; ++i ) {
-            for ( int j = 0; j < cols; ++j ) {
-                result[i][j] = (*this)[i][j] * a;
-            }
-        }
-        return result;
-    }
-    Matrix operator* ( int a ) {
-        Matrix result(rows, cols);
-        for ( int i = 0; i < rows; ++i ) {
-            for ( int j = 0; j < cols; ++j ) {
-                result[i][j] = (*this)[i][j] * a;
-            }
-        }
-        return result;
-    }
-    Matrix operator* ( Matrix second ) {
-        if ( cols != second.rows )
-            throw "SIZE_NO_MATCH";
-        Matrix result( rows, second.cols );
-        for ( int i = 0; i < result.rows; ++i ) {
-            for ( int j = 0; j < result.cols; ++j ) {
-                U sum{};
-                for ( int k = 0; k < cols; ++k ) {
-                    sum += matrix[i][k] * second[k][j];
-                }
-                result[i][j] = sum;
-            }
-        }
-        return result;
-    }
-    Matrix operator+ ( Matrix second ) {
-        if ( rows != second.rows && cols != second.cols )
-            throw "SIZE_NO_MATCH";
-
-        Matrix result(rows, cols);
-        for ( int i = 0; i < rows; ++i ) {
-            for ( int j = 0; j < cols; ++j ) {
-                result[i][j] = (*this)[i][j] + second[i][j];
-            }
-        }
-        return result;
-    }
-    Matrix operator- ( Matrix second ) {
-        if ( rows != second.rows && cols != second.cols )
-            throw "SIZE_NO_MATCH";
-            
-        Matrix result(rows, cols);
-        for ( int i = 0; i < rows; ++i ) {
-            for ( int j = 0; j < cols; ++j ) {
-                result[i][j] = (*this)[i][j] - second[i][j];
-            }
-        }
-        return result;
-    }
+    //map all elements with [](){} lambda function
     template<class Function>
-    Function map(Function f) {
-        for ( auto& row : matrix ) 
-            for ( auto& value : row )
-                f( value );
-        return f;
-    }
-
-    friend std::ostream& operator<< ( std::ostream& os, const Matrix& target ) {
-        for ( auto& row : target.matrix ) {
-            for ( auto& value : row )
-                os << value << "\t";
-            os << "\n";
-        }
-        return os;
-    }
+    Function map(Function);
 };
+
+template <class U>
+Matrix<U>::Matrix( std::initializer_list< std::initializer_list<U> > content ) {
+    rows = content.size();
+    cols = content.begin() -> size();
+    matrix.resize(rows);
+    auto j = content.begin();
+    for ( int i = 0; i < rows; ++i, ++j )
+        matrix[i].assign( *j );
+}
+
+template <class U>
+Matrix<U>::Matrix( int rows_, int cols_ ) {
+    rows = rows_;
+    cols = cols_;
+
+    matrix.resize( rows );
+    for ( auto& row : matrix )
+        row.resize( cols );
+}
+
+template <class U>
+std::pair<int, int> Matrix<U>::getSize() {
+    return std::pair(rows,cols);
+}
+
+template <class U>
+void Matrix<U>::reshape( int n, int m ) {
+    rows = n;
+    cols = m;
+    matrix.resize(n);
+    for ( auto& row : matrix )
+        row.resize(m);
+}
+
+template <class U>
+void Matrix<U>::fill( U value ) {
+    for ( auto& row : matrix )
+        for ( auto& val : row )
+            val = value;
+}
+
+template <class U>
+void Matrix<U>::randomize( U upper_bound, U lower_bound ) {
+    for ( auto& row : matrix )
+        for ( auto& value : row )
+            value = float(rand()%1000) / 1000 * (upper_bound - lower_bound) + lower_bound;
+}
+
+template <class U>
+void Matrix<U>::transpose() {
+    Matrix result(cols, rows);
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            result[j][i] = (*this)[i][j];
+        }
+    }
+    *this = result;
+}
+
+template <class U>
+Matrix<U> Matrix<U>::T() {
+    auto result = *this;
+    result.transpose();
+    return result;
+}
+
+template <class U>
+std::vector<U>& Matrix<U>::operator[] ( unsigned int n ) {
+    return matrix[n];
+}
+
+template <class U>
+Matrix<U> Matrix<U>::operator* ( float a ) {
+    Matrix result(rows, cols);
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            result[i][j] = (*this)[i][j] * a;
+        }
+    }
+    return result;
+}
+
+template <class U>
+Matrix<U> Matrix<U>::operator* ( int a ) {
+    Matrix result(rows, cols);
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            result[i][j] = (*this)[i][j] * a;
+        }
+    }
+    return result;
+}
+
+template <class U>
+Matrix<U> Matrix<U>::operator* ( Matrix second ) {
+    if ( cols != second.rows )
+        throw "SIZE_NO_MATCH";
+    Matrix result( rows, second.cols );
+    for ( int i = 0; i < result.rows; ++i ) {
+        for ( int j = 0; j < result.cols; ++j ) {
+            U sum{};
+            for ( int k = 0; k < cols; ++k ) {
+                sum += matrix[i][k] * second[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+}
+
+template <class U>
+Matrix<U> Matrix<U>::operator+ ( Matrix second ) {
+    if ( rows != second.rows && cols != second.cols )
+        throw "SIZE_NO_MATCH";
+
+    Matrix result(rows, cols);
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            result[i][j] = (*this)[i][j] + second[i][j];
+        }
+    }
+    return result;
+}
+
+template <class U>
+Matrix<U> Matrix<U>::operator- ( Matrix second ) {
+    if ( rows != second.rows && cols != second.cols )
+        throw "SIZE_NO_MATCH";
+        
+    Matrix result(rows, cols);
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            result[i][j] = (*this)[i][j] - second[i][j];
+        }
+    }
+    return result;
+}
+
+template <class U>
+std::ostream& operator<< ( std::ostream& os, const Matrix<U>& target ) {
+    for ( auto& row : target.matrix ) {
+        for ( auto& value : row )
+            os << value << "\t";
+        os << "\n";
+    }
+    return os;
+}
+
+template<class U>
+template<class Function>
+Function Matrix<U>::map(Function f) {
+    for ( auto& row : matrix ) 
+        for ( auto& value : row )
+            f( value );
+    return f;
+}
 
 #endif
